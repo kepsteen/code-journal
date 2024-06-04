a1321876b90ec6dc38e599e3daab479319ca1f89
Cody Epstein <codyepstein@gmail.com>
2024-06-04 16:40:34 +0000

    Cleaned up css files. Moved non utility styles to styles.css. Event listener changed to listen for form submit

--- ts/main.ts
+++ ts/main.ts
@@ -6,22 +6,22 @@ interface Entry {
   entryId: number;
 }
 
+interface FormElements extends HTMLFormControlsCollection {
+  title: HTMLInputElement;
+  photoURL: HTMLInputElement;
+  notes: HTMLTextAreaElement;
+}
+
 const $photoURL = document.querySelector('#photo-url') as HTMLInputElement;
 const $newEntryImage = document.querySelector(
   '#new-entry-image',
 ) as HTMLImageElement;
-const $saveBtn = document.querySelector('#save-btn') as HTMLButtonElement;
-const $title = document.querySelector('#title') as HTMLInputElement;
-const $notes = document.querySelector('#notes') as HTMLTextAreaElement;
 const $newEntryForm = document.querySelector(
   '#new-entry-form',
 ) as HTMLFormElement;
 
 if (!$photoURL) throw new Error('no photoURL input found');
 if (!$newEntryImage) throw new Error('no image found');
-if (!$saveBtn) throw new Error('no save button found');
-if (!$title) throw new Error('no title input found');
-if (!$notes) throw new Error('no notes input found');
 if (!$newEntryForm) throw new Error('no form element found');
 
 $photoURL.addEventListener('input', () => {
@@ -29,12 +29,14 @@ $photoURL.addEventListener('input', () => {
   $newEntryImage.setAttribute('src', photoURL);
 });
 
-$saveBtn.addEventListener('click', (event: Event) => {
+$newEntryForm.addEventListener('submit', (event: Event) => {
   event.preventDefault();
+
+  const $formElements = $newEntryForm.elements as FormElements;
   const entry: Entry = {
-    title: $title.value,
-    photoURL: $photoURL.value,
-    notes: $notes.value,
+    title: $formElements.title.value,
+    photoURL: $formElements.photoURL.value,
+    notes: $formElements.notes.value,
     entryId: data.nextEntryId,
   };
   data.entries.unshift(entry);
